acd65bab5109a7d3dc99b618c96482306ba87eb6
GRACY KUREEL <graciekureel1@gmail.com>
2021-06-29 04:17:00 +0530

    Scratcheffect

---
+++ app/src/main/java/com/example/scratchscreen/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.scratchscreen;
+
+import android.os.Bundle;
+
+import com.anupkumarpanwar.scratchview.ScratchView;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.scratchscreen.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ScratchView scratchView = findViewById(R.id.scratchView);
+    }
+}
+
+
